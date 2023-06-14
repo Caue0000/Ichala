a90d03e97f1530cf037f695754feb323f8ca57f5
Caue <caue12.giovaz@gmail.com>
2023-06-14 10:29:20 -0300

    primeiro commit

---
+++ src/entidade/Cidade.java
@@ -0,0 +1,39 @@
+package entidade;
+
+/**
+ *
+ * @author mateus
+ */
+public class Cidade {
+    private int id;
+    private String descricao;
+    private int estadoId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getEstadoId() {
+        return estadoId;
+    }
+
+    public void setEstadoId(int estadoId) {
+        this.estadoId = estadoId;
+    }
+
+
+    
+    
+}
